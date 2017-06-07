878fc5762cb5992bfa45ec7eda991c096b67af7e
Alex Roan <alex.roan@hotmail.com>
2017-06-07 08:59:47 +0100

    Basic printing of fixtures per group

--- js/predictor.js
+++ js/predictor.js
@@ -1,7 +1,25 @@
 $(window).ready(function(){
 		
 	$.getJSON("tournament.json", function(data){
+		var mainContent = $("#main-content");
 		console.log(data);
+		groups = data["Groups"];
+		map = data["TeamNames"]
+		for (var group in groups){
+			fixtures = groups[group]["Fixtures"];
+			mainContent.append("<h4>Group "+group+"</h4>")
+			for (var fixture in fixtures){
+				var home = fixtures[fixture]["Home"];
+				if (home in map){
+					home = map[home];
+				}
+				var away = fixtures[fixture]["Away"];
+				if (away in map){
+					away = map[away];
+				}
+				mainContent.append("<p><strong>Match "+fixture+":</strong> "+home+" v "+away+"</p>")
+			}
+		}
 	});
 	
 });
