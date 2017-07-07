9993a24d9b6e35aa98717c16657b0d8c0ab4cbe3
Alex Roan <alex.roan@hotmail.com>
2017-07-07 16:06:38 +0100

    Added group position points calculations
    Messy though

--- server/applyresults.py
+++ server/applyresults.py
@@ -27,7 +27,7 @@ def find_match_in_predictions(predictions, match_number):
 	return returned
 
 
-def calculate_points(fixture_prediction, fixture_result):
+def calculate_fixture_prediction_points(fixture_prediction, fixture_result):
 	points = 0
 	home_goals_prediction = fixture_prediction["HomeGoals"]
 	away_goals_prediction = fixture_prediction["AwayGoals"]
@@ -83,6 +83,7 @@ def update_table(table, home, home_goals, away, away_goals):
 
 
 
+
 print('Reading real model')
 real_model = None
 real_groups = None
@@ -124,14 +125,42 @@ if real_model is not None and real_groups is not None:
 			if int(result_id) < 49:
 				fixture_result = real_model[result_id]
 				fixture_prediction = find_match_in_predictions(user_model, result_id)
-				points = calculate_points(fixture_prediction, fixture_result)
+				points = calculate_fixture_prediction_points(fixture_prediction, fixture_result)
 				fixture_prediction["Points"] = points
 				total_points = total_points + points
-		user_model["TotalPoints"] = total_points
 		print('Group fixture points calculated')
 
 		#Group position points
-		
+		print('Calculating group position points')
+		total_groups_points = 0
+		for group_id in real_groups:
+			group_points = 0
+			group = real_groups[group_id]["Positions"]
+			#if group has teams in it
+			if group and len(group) == 4:
+				for team_id in group:
+					group[team_id]["Team"] = team_id
+				real_positions = sorted(group.values(), key=operator.itemgetter('Pts', 'GD', 'GF', 'GA'), reverse=True)
+				predicted_group = user_model["Groups"][group_id]
+				predicted_group_teams = predicted_group["Teams"]
+				for team_id in predicted_group_teams:
+					predicted_group_teams[team_id]["Team"] = team_id
+				predicted_positions = sorted(predicted_group_teams.values(), key=operator.itemgetter('Pts', 'GD', 'GF', 'GA'), reverse=True)
+				i=0
+				while i < 4:
+					real_team = real_positions[i]["Team"]
+					predicted_team = predicted_positions[i]["Team"]
+					print ("Position %d predicted: %s, actual: %s" % (i+1, predicted_team, real_team))
+					if real_positions[i]["Team"] == predicted_positions[i]["Team"]:
+						group_points += 3
+					i += 1
+				predicted_group["PositionPoints"] = group_points
+				print("Group %s points: %d" % (group_id, group_points))
+				total_groups_points += group_points
+
+		user_model["Groups"]["Points"] = total_groups_points
+		total_points += total_groups_points
+		user_model["TotalPoints"] = total_points
 
 		#Knockout fixture points (including extra points for having correct team in game)
 		#Tournament winner, runner up, 3rd and 4th place points
