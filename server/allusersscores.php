056e14670f6beb0eaac6b91fc4ec481ae8367e76
Alex Roan <alex.roan@hotmail.com>
2017-08-28 21:51:45 +0200

    Server endpoint for user scores

---
+++ server/allusersscores.php
@@ -0,0 +1,21 @@
+<?php
+
+$directory = "predictions/test/";
+$files = glob($directory."*.json");
+
+$data = array();
+
+for ($i=0; $i < count($files); $i++) { 
+	$thisfile = $files[$i];
+	$json_string = file_get_contents($thisfile);
+
+	$decoded_json = json_decode($json_string);
+	$score = $decoded_json->TotalPoints;
+
+	$filedata["user"] = $thisfile;
+	$filedata["score"] = $score;
+
+	array_push($data, $filedata);
+}
+
+echo json_encode($data);
